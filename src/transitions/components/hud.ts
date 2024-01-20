74084afd19250448d45e5a9e14f42d07a7d9202f
Lee <seungjun.lee@ecubelabs.com>
2024-01-20 14:06:04 +0900

    chore: fade

--- src/transitions/components/hud.ts
+++ src/transitions/components/hud.ts
@@ -3,12 +3,9 @@ import { gsap } from "gsap";
 const hudScene = document.querySelector("#hud-webgl")!;
 
 export const initHud = async (title?: ArtworkTitle) => {
-  
-
   const tl = gsap.timeline();
   gsap.killTweensOf(hudScene);
 
-  // TODO: 너무 빨리 사라지는데?
   tl.to(hudScene, {
     autoAlpha: 1,
     duration: 0.5,
@@ -26,7 +23,7 @@ export const finishHud = () => new Promise((resolve) => {
 
   tl.to(hudScene, {
     autoAlpha: 0,
-    duration: 0.3,
+    duration: 0.5,
     ease: "power1.out",
   });
 });
