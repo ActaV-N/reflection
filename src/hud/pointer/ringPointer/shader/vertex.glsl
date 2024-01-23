246f3d310445a45108cb39f76b98f43091a7f4d4
Lee <seungjun.lee@ecubelabs.com>
2024-01-23 17:43:20 +0900

    chore: circle

--- src/hud/pointer/ringPointer/shader/vertex.glsl
+++ src/hud/pointer/ringPointer/shader/vertex.glsl
@@ -1,7 +1,7 @@
 #ifdef GL_ES
 precision lowp float;
 #endif
-uniform float uTime;
+
 uniform float uScale;
 
 varying vec3 vPosition;
@@ -9,36 +9,6 @@ varying vec3 vNewPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
 
-vec3 hash33(vec3 p3)
-{
-	p3 = fract(p3 * vec3(.1031,.11369,.13787));
-    p3 += dot(p3, p3.yxz+19.19);
-    return -1.0 + 2.0 * fract(vec3(p3.x+p3.y, p3.x+p3.z, p3.y+p3.z)*p3.zyx);
-}
-float snoise3(vec3 p)
-{
-    const float K1 = 0.333333333;
-    const float K2 = 0.166666667;
-    
-    vec3 i = floor(p + (p.x + p.y + p.z) * K1);
-    vec3 d0 = p - (i - (i.x + i.y + i.z) * K2);
-    
-    vec3 e = step(vec3(0.0), d0 - d0.yzx);
-	vec3 i1 = e * (1.0 - e.zxy);
-	vec3 i2 = 1.0 - e.zxy * (1.0 - e);
-    
-    vec3 d1 = d0 - (i1 - K2);
-    vec3 d2 = d0 - (i2 - K1);
-    vec3 d3 = d0 - 0.5;
-    
-    vec4 h = max(0.6 - vec4(dot(d0, d0), dot(d1, d1), dot(d2, d2), dot(d3, d3)), 0.0);
-    vec4 n = h * h * h * h * vec4(dot(d0, hash33(i)), dot(d1, hash33(i + i1)), dot(d2, hash33(i + i2)), dot(d3, hash33(i + 1.0)));
-    
-    return dot(vec4(31.316), n);
-}
-
-#define time uTime
-float noiseScale = 0.65;
 
 void main() {
   vPosition = vPosition;
@@ -46,7 +16,8 @@ void main() {
   vUv = uv;
 
   // MVP
-  vec3 newPosition = position * (snoise3(vec3(uv * noiseScale, uTime * 1.5)) * 0.5 + 1.5) * uScale * 0.6;
+  
+  vec3 newPosition = position * uScale * 0.6;
   vNewPosition = newPosition;
   vec4 modelViewPosition = modelViewMatrix * vec4(newPosition, 1.0);
   gl_Position = projectionMatrix * modelViewPosition;
--- src/transitions/components/hud.ts
+++ src/transitions/components/hud.ts
@@ -2,7 +2,7 @@ import { gsap } from "gsap";
 
 const hudScene = document.querySelector("#hud-webgl")!;
 
-export const initHud = async (title?: ArtworkTitle) => {
+export const initHud = async () => {
   const tl = gsap.timeline();
   gsap.killTweensOf(hudScene);
 
@@ -22,6 +22,7 @@ export const finishHud = () => new Promise((resolve) => {
   gsap.killTweensOf(hudScene);
 
   tl.to(hudScene, {
+    delay: 0.2,
     autoAlpha: 0,
     duration: 0.5,
     ease: "power1.out",
