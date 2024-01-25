e66c0926adcf536ec64729b971ee1d7f619dccab
actav-n <gksdmltk02@gmail.com>
2024-01-25 23:10:27 +0900

    fix

--- src/transitions/common/index.ts
+++ src/transitions/common/index.ts
@@ -1,17 +1,20 @@
-import { finishControlPanel, finishHud, initControlPanel, initHud } from "../components";
+import {
+  finishControlPanel,
+  finishHud,
+  initControlPanel,
+  initHud,
+} from "../components";
 
-export const commonInitializer = (fn: (...args: any[]) => Promise<any>, title?: ArtworkTitle, ) => async (...args: any[]) => {
-  await Promise.all([
-    await initControlPanel(title),
-    await initHud(title),
-  ])
-  await fn(args);
-}
+export const commonInitializer =
+  (fn: (...args: any[]) => Promise<any>, title?: ArtworkTitle) =>
+  async (...args: any[]) => {
+    await Promise.all([await initControlPanel(title), await initHud()]);
+    await fn(args);
+  };
 
-export const commonFinisher = (fn: (...args: any[]) => Promise<any>, title?: ArtworkTitle, ) => async (...args: any[]) => {
-  await Promise.all([
-    finishControlPanel(),
-    finishHud()
-  ])
-  await fn(args);
-}
+export const commonFinisher =
+  (fn: (...args: any[]) => Promise<any>, title?: ArtworkTitle) =>
+  async (...args: any[]) => {
+    await Promise.all([finishControlPanel(), finishHud()]);
+    await fn(args);
+  };
