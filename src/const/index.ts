1d680785b7307515d378a072b7b70bb13d8e24d7
actav-n <gksdmltk02@gmail.com>
2024-01-30 23:09:39 +0900

    chore: pointer

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,7 +1,7 @@
 export const GESTURE = {
-  NONE: 'None',
-  CLOSED: 'Closed_Fist',
-  OPEN: 'Open_Palm',
-}
+  NONE: "None",
+  CLOSED: "Closed_Fist",
+  OPEN: "Open_Palm",
+};
 
-export const IPAD_CONST = /iPad|iPhone|iPod/.test(navigator.platform) ? 3.0 : 1;
+export const IPAD_CONST = /iPad|iPhone|iPod/.test(navigator.platform) ? 6.0 : 1;
